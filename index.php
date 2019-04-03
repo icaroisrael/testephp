c3483f9acaad6db53b18a4bbc6f885cb69b6db62
Israel Furtado <icaroisrael@hotmail.com>
2019-04-02 21:16:29 -0300

    Segunda Versão

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 
 <form action="index.php" method="POST">
-Login<input type="email" name="login"><br>
+Login<input type="text" name="login"><br>
 Senha<input type="password" name="senha"><br>
 <input type="submit" value="Enviar">
 </form>
